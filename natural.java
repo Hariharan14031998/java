dab53c7ee7926486b1a4aa4ba0513d7b2e8b4f91
Hariharan14031998 <43901406+Hariharan14031998@users.noreply.github.com>
2019-02-12 15:48:07 +0530

    Add files via upload

---
+++ natural.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class natural  
+
+{
+    
+public static void main(String args[])
+    
+{
+        
+int x, i = 1 ,sum = 0;
+        
+System.out.println("Enter Number :");
+        
+Scanner s = new Scanner(System.in);
+        
+x = s.nextInt();
+        
+while(i <= x)
+        
+{
+            
+sum = sum +i;
+            
+i++;
+        
+}
+        
+System.out.println("Sum of "+x+" numbers is :"+sum);
+    
+} 
+}
