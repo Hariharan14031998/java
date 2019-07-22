74e4db5289bcd90bc20aa849bd4bec61bb49a745
Hariharan14031998 <43901406+Hariharan14031998@users.noreply.github.com>
2019-07-22 19:59:50 +0530

    Update count.java

--- count.java
+++ count.java
@@ -7,7 +7,7 @@ int num, Count=0;
 Scanner sc = new Scanner(System.in);		
 System.out.println("\n Please Enter any Number: ");
 num = sc.nextInt();
-while(Number > 0) 
+while(num > 0) 
 {
 num = num / 10;
 Count = Count + 1; 
